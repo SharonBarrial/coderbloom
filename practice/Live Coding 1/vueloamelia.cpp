66d877dfe019d48d8b4104019a715da7837dad33
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-27 19:55:43 -0500

    feat: create solution vuelo amelia

---
+++ practice/Live Coding 1/vueloamelia.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin>> n;
+    if (n>= 0 && n<= 1000){
+        int total;
+    total = 12 * n;
+    cout << total;
+    }
+    else { cout << "Fuera de rango";}
+
+}
